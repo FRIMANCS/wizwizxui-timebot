3c8473ab613cd47d6ace7fcd1242441419b46a2c
FRIMAN <102473126+FRIMANCS@users.noreply.github.com>
2025-11-22 12:33:44 -0800

    Update values.php

--- settings/values.php
+++ settings/values.php
@@ -368,6 +368,10 @@ $buttonValues = [
     'disable_config'=>"غیر فعال سازی کانفیگ",
     "tron_gateway"=>"درگاه ترون",
     'plan_discount'=>"روی پلن",
+     'apple_add'=>"➕ Add Apple ID",
+    'apple_manage'=>"✏️ Edit / ❌ Remove Apple ID",
+    'apple'=>"🛒 Apple ID",
+    'myapple'=>"🍎 Apple ID",
     'server_discount'=>"روی سرور"
 ];
 ?>
